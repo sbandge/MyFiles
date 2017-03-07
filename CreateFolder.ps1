7a9505fd3664282538fd10056038701eee59921f
sbandge <sbandge@hotmail.com>
2017-03-07 08:51:05 +0000

    For Shared Folder

---
+++ CreateFolder.ps1
@@ -0,0 +1,6 @@
+﻿ # Configure SMB Share and Set permissions
+    New-Item -ItemType Directory -Force -Path "C:\tmp\Share" 
+    if(!(Get-SMBShare -Name "Share" -ea 0)){
+          New-SMBShare -Name "Share" -Path "C:\tmp\Share" -Description "A Share"
+    }
+    Get-SmbShareAccess -Name "Share" | Grant-SmbShareAccess -AccountName Everyone -AccessRight Full -Force
